43558230aa682ebaa58ccf03c640f6164f2b234b
Josh Bennett <joshbennett793@yahoo.com>
2023-05-10 16:15:59 -0500

    Add createRecipe function

--- src/meal.js
+++ src/meal.js
@@ -25,11 +25,19 @@ function decreasePrice(price) {
 	return price *= .90;
 }
 
+function createRecipe(title, ingredients, menuItemType) {
+	return {
+		title: title,
+		ingredients: ingredients,
+		type: menuItemType,
+	}
+}
+
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
   formatPrice,
   decreasePrice,
-  // createRecipe
+  createRecipe
 };
