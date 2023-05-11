6b5696d23e7181a4dbcb54b08b4bc962e664b0ed
Josh Bennett <joshbennett793@yahoo.com>
2023-05-11 12:18:45 -0500

    Add refundOrder function

--- src/order.js
+++ src/order.js
@@ -4,9 +4,17 @@ function takeOrder(order, deliveryOrders) {
   }
 }
 
+function refundOrder(orderNumber, orders) {
+	for (var i = 0; i < orders.length; i++) {
+		if (orders[i].orderNumber === orderNumber) {
+			return orders.splice(i, 1)
+		}
+	}
+}
+
 module.exports = {
   takeOrder,
-  // refundOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 };
