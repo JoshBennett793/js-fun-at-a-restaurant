9ebb2edbc189351b5ed207c5161f5a871e2c7944
Josh Bennett <joshbennett793@yahoo.com>
2023-05-11 21:16:07 -0500

    Add addMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -9,9 +9,15 @@ function createRestaurant(name) {
   };
 }
 
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item)) {
+    restaurant.menus[item.type].push(item);
+  }
+}
+
 module.exports = {
   createRestaurant,
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem,
   // checkForFood
 };
