e81bf7f2a92577e4e6db7a755da2d04f8e8129e3
Josh Bennett <joshbennett793@yahoo.com>
2023-05-12 09:54:09 -0500

    Add removeMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -15,9 +15,20 @@ function addMenuItem(restaurant, item) {
   }
 }
 
+function removeMenuItem(restaurant, item, type) {
+	var menu = restaurant.menus[type];
+  for (var i = 0; i < menu.length; i++) {
+    if (menu[i].name === item) {
+      menu.splice(menu.indexOf(item), 1);
+      return `No one is eating our ${item} - it has been removed from the ${type} menu!`;
+    }
+  }
+	return `Sorry, we don't sell ${item}, try adding a new recipe!`;
+}
+
 module.exports = {
   createRestaurant,
   addMenuItem,
-  // removeMenuItem,
+  removeMenuItem,
   // checkForFood
 };
